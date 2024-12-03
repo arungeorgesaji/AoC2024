56f2b9ff3c38b3119fedccde2777b981a321f5ec
arungeorgesaji <arungeorgesaji@gmail.com>
2024-12-03 18:29:07 +0530

    Completed part2 of day2 and I had forgot to commit

--- day2/part2/main.cpp
+++ day2/part2/main.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool is_safe(vector<int> v) {
+    bool inc = true;
+    bool dec = true;
+    for(int i=1;i<v.size();i++) {
+        if(abs(v[i] - v[i-1]) < 1 || abs(v[i] - v[i-1]) > 3) return false;
+        if(v[i] < v[i-1]) inc = false;
+        if(v[i] > v[i-1]) dec = false;
+    }
+
+    return inc || dec;
+}
+
 int main() {
 
     ifstream f("input.txt");
-    unordered_multiset<int> nums1;
-    unordered_multiset<int> nums2;
 
     if (!f.is_open()) {
         cerr << "Error opening the file!";
@@ -13,31 +23,30 @@ int main() {
     }
 
     string s;
+    int safe = 0;
 
     while (getline(f, s)){
         istringstream iss(s);
-        bool first = true;
-        while (iss >> s){
-            if (first){
-                nums1.insert(stoi(s));
-                first = false;
-            }else {
-                nums2.insert(stoi(s));
-                first = true;
+        vector<int> v;
+        int x;
+        while (iss >> x) v.push_back(x);
+
+        if(is_safe(v)) {
+            safe++;
+            continue;
+        }
+
+        for(int i=0;i<v.size();i++) {
+            vector<int> temp = v;
+            temp.erase(temp.begin() + i);
+            if (is_safe(temp)) {
+                safe++;
+                break;
             }
         }
     }
     
-    auto it1 = nums1.begin();
-
-    int score = 0;
-
-    while (it1 != nums1.end()){
-        score += *it1 * nums2.count(*it1); 
-        it1++;
-    }
-
-    cout << score << endl;
+    cout << safe << endl;
 
     f.close();
     return 0;
