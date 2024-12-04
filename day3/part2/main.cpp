88d4631dd510058b6cafe57349cc6d9dc28014ad
arungeorgesaji <arungeorgesaji@gmail.com>
2024-12-04 19:36:39 +0530

    Completed part2 of day3

--- day3/part2/main.cpp
+++ day3/part2/main.cpp
@@ -1,18 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool is_safe(vector<int> v) {
-    bool inc = true;
-    bool dec = true;
-    for(int i=1;i<v.size();i++) {
-        if(abs(v[i] - v[i-1]) < 1 || abs(v[i] - v[i-1]) > 3) return false;
-        if(v[i] < v[i-1]) inc = false;
-        if(v[i] > v[i-1]) dec = false;
-    }
-
-    return inc || dec;
-}
-
 int main() {
 
     ifstream f("input.txt");
@@ -23,30 +11,54 @@ int main() {
     }
 
     string s;
-    int safe = 0;
+    int sum = 0;
+    bool current_do = true;
+
+    regex pattern(R"(mul\((\d{1,}),(\d{1,})\))");
+    regex do_pattern(R"(do\(\))");
+    regex dont_pattern(R"(don't\(\))");
 
     while (getline(f, s)){
-        istringstream iss(s);
-        vector<int> v;
-        int x;
-        while (iss >> x) v.push_back(x);
+        sregex_iterator it(s.begin(), s.end(), pattern);
+        sregex_iterator do_it(s.begin(), s.end(), do_pattern);
+        sregex_iterator dont_it(s.begin(), s.end(), dont_pattern);
+        sregex_iterator end;
 
-        if(is_safe(v)) {
-            safe++;
-            continue;
+        vector<pair<int, string>> events;
+
+        for (; it != end; ++it) {
+            events.emplace_back(it->position(0), (*it).str());
         }
 
-        for(int i=0;i<v.size();i++) {
-            vector<int> temp = v;
-            temp.erase(temp.begin() + i);
-            if (is_safe(temp)) {
-                safe++;
-                break;
+        for (; do_it != end; ++do_it) {
+            events.emplace_back(do_it->position(0), "do");
+        }
+
+        for (; dont_it != end; ++dont_it) {
+            events.emplace_back(dont_it->position(0), "dont");
+        }
+          
+        sort(events.begin(), events.end());;
+
+        for (auto const[pos, event] : events) {
+            if (event == "do") {
+                current_do = true;
+            } else if (event == "dont") {
+                current_do = false;
+            } else {
+                if (current_do) {
+                    smatch match;
+                    regex_match(event, match, pattern);
+                    sum += stoi(match[1]) * stoi(match[2]);
+                }
             }
         }
+
+
+
     }
     
-    cout << safe << endl;
+    std::cout << sum << std::endl;
 
     f.close();
     return 0;
